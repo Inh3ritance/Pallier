25a3b297bf0b453d41fe5af8103c457d65f8bed2
Inh3ritance <herreralaces@gmail.com>
2020-10-24 16:39:36 -0700

    Updates

---
+++ precache-manifest.af553fdb623b3df36f76d853d17dadc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd09762e68a6c75f5aa86e63b6c42fbe",
+    "url": "/Pallier/index.html"
+  },
+  {
+    "revision": "6e58df577e286ec33ceb",
+    "url": "/Pallier/static/css/main.4b2ef2fe.chunk.css"
+  },
+  {
+    "revision": "9fce289e572f22ee7046",
+    "url": "/Pallier/static/js/2.867bdd87.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Pallier/static/js/2.867bdd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e58df577e286ec33ceb",
+    "url": "/Pallier/static/js/main.d00f72cf.chunk.js"
+  },
+  {
+    "revision": "232640421e75072187c4",
+    "url": "/Pallier/static/js/runtime-main.f2dd6766.js"
+  }
+]);
